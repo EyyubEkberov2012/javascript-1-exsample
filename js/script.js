0b9e8a808e16723a166d8b83b7a01307239cb66c
Əyyub Əkbərov <yyubekber4@gmail.com>
2024-11-01 19:19:13 +0400

    final

---
+++ js/script.js
@@ -0,0 +1,24 @@
+function batman(){
+    var script = document.getElementById('img');
+    script.src = 'https://images7.alphacoders.com/134/1340753.png';
+}
+function spiderman(){
+    var script = document.getElementById('img');
+    script.src = 'https://free-3dtextureshd.com/wp-content/uploads/2024/05/258.jpg.webp';
+}
+function superman(){
+    var script = document.getElementById('img');
+    script.src = 'https://c4.wallpaperflare.com/wallpaper/353/690/516/superman-batman-v-superman-dawn-of-justice-man-of-steel-dc-comics-wallpaper-preview.jpg';
+}
+function deadpool(){
+    var script = document.getElementById('img');
+    script.src = 'https://images3.alphacoders.com/135/1350860.png';
+}
+function Hulk(){
+    var script = document.getElementById('img');
+    script.src = 'https://i.ytimg.com/vi/dfmTSkDvf78/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLBuK_-al5xsEqZkuw29Wb-yNbhvJQ';
+}
+function america(){
+    var script = document.getElementById('img');
+    script.src = 'https://images2.alphacoders.com/110/thumb-1920-1105688.jpg';
+}
